fb16f2b5ec68321090808a71edd9b625adb13b86
abassGarane <thelibertinewreck@gmail.com>
2024-09-07 12:41:25 +0300

    stack that works for both ints and strings using generics

--- stack/stack.go
+++ stack/stack.go
@@ -1,39 +1,39 @@
 package stack
 
-type Stack []int
+type Stack[T int | string] []T
 
 // Push method to add an element to the stack
-func (s *Stack) Push(value int) {
+func (s *Stack[T]) Push(value T) {
 	*s = append(*s, value)
 }
 
 // Pop method to remove and return the top element from the stack
-func (s *Stack) Pop() (int, bool) {
+func (s *Stack[T]) Pop() (item T, ok bool) {
 	if len(*s) == 0 {
-		return 0, false // return zero value and false if stack is empty
+		return item, false // return zero value and false if stack is empty
 	}
 
 	// Get the top element
 	index := len(*s) - 1
-	element := (*s)[index]
+	item = (*s)[index]
 
 	// Remove the top element
 	*s = (*s)[:index]
-	return element, true
+	return item, true
 }
 
 // Peek method to return the top element without removing it
-func (s *Stack) Peek() (int, bool) {
+func (s *Stack[T]) Peek() (item T, ok bool) {
 	if len(*s) == 0 {
-		return 0, false // return zero value and false if stack is empty
+		return item, false // return zero value and false if stack is empty
 	}
 
 	// Get the top element
 	index := len(*s) - 1
-	element := (*s)[index]
-	return element, true
+	item = (*s)[index]
+	return item, true
 }
 
-func (s *Stack) length() int {
+func (s *Stack[T]) length() int {
 	return len(*s)
 }
