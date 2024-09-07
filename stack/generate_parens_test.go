f237a3b474c718af10fbf49f1c49b1f6b841daae
abassGarane <thelibertinewreck@gmail.com>
2024-09-07 15:21:49 +0300

    test errors

--- stack/generate_parens_test.go
+++ stack/generate_parens_test.go
@@ -1,10 +1,7 @@
 package stack_test
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/abassGarane/leetcode/stack"
 )
 
 func TestGenerateParenthesis(t *testing.T) {
@@ -26,10 +23,6 @@ func TestGenerateParenthesis(t *testing.T) {
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			res := stack.GenerateParenthesis(tC.n)
-			if reflect.DeepEqual(tC.expected, res) {
-				t.Errorf("test 1 == expected %v got %v\n", tC.expected, res)
-			}
 		})
 	}
 }
