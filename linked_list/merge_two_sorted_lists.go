f74e253ee3b2755ded7e2ff90dd0650becfb8733
abassGarane <thelibertinewreck@gmail.com>
2024-09-23 13:44:23 +0300

    some functional changes

--- linked_list/merge_two_sorted_lists.go
+++ linked_list/merge_two_sorted_lists.go
@@ -1,23 +1,23 @@
 package linked_list
 
-func Merge(l1 *ListNode, l2 *ListNode) *ListNode {
+func Merge(list1 *ListNode, list2 *ListNode) *ListNode {
 	head := &ListNode{}
 	current := head
-	for l1 != nil && l2 != nil {
-		if l1.Val < l2.Val {
-			current.Next = l1
-			current = l1
-			l1 = l1.Next
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			current = list1
+			list1 = list1.Next
 		} else {
-			current.Next = l2
-			current = l2
-			l2 = l2.Next
+			current.Next = list2
+			current = list2
+			list2 = list2.Next
 		}
 	}
-	if l1 != nil {
-		current.Next = l1
+	if list1 != nil {
+		current.Next = list1
 	} else {
-		current.Next = l2
+		current.Next = list2
 	}
 	return head.Next
 }
